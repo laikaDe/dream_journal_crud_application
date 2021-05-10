1e25d9024a8fa2b560c6eac585a37e1f516c8176
Laika Deunert <laikadeunert@Laikas-Laptop.local>
2021-05-10 00:21:53 -0700

    1. Fixed the issue with all journals being accessible in every account

--- app/controllers/journal_controller.rb
+++ app/controllers/journal_controller.rb
@@ -8,24 +8,38 @@ class JournalController < ApplicationController
     end
 
     post '/journals' do
-        @journal = Journal.create(
+        
+        @user = User.find(session[:user_id])
+        binding.pry
+        if @user
+            @journal = Journal.create(
             title: params[:title], 
+            user_id: session[:user_id],
             date: params[:date], 
             entry: params[:entry], 
             mood: params[:mood]
             )
-        redirect "journals/#{@journal.id}"
+            redirect "journals/#{@journal.id}"
+        else
+            redirect "/login"
+        end
     end
 
     #read 
 
     get '/journals/:id' do
-        @journal = Journal.find(params[:id])
+        @journal = Journal.find_by_id(params[:id])
         erb :'/journals/show'
     end
 
     get '/journals' do 
+        #Iterate through all journals and filter 
+        #the journals with the current user id
+        #1. Iterate through all journals
+        #2. Filter out the current user id 
+        #3. Store the journals with the current user id in a variable
         @journals = Journal.all
+        @journals = @journals.select {|journal|journal.user_id == session[:user_id]}
         erb :'/journals/index'
         
         #returns an array of all instances of this class
@@ -34,7 +48,7 @@ class JournalController < ApplicationController
     #update 
 
     get '/journals/:id/edit' do 
-        @journal = Journal.find(params[:id])
+        @journal = Journal.find_by_id(params[:id])
         erb :'/journals/edit'
     end
 
@@ -42,7 +56,6 @@ class JournalController < ApplicationController
         @journal = Journal.find(params[:id])
         @journal.update(
             title: params[:title],
-            by: params[:user_id], 
             date: params[:date], 
             entry: params[:entry], 
             mood: params[:mood]
